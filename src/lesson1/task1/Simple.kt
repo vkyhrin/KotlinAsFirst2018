30db62a737f2b288b32396008a33eff3d5a174ee
vkyhrin <vkyhrin@gmail.com>
2018-09-25 00:30:40 +0300

    сделал одну задачку из 2 лессона

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -75,7 +75,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (sagenes
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = grad*PI/180+min*PI/180/60+sec*PI/180/60/60
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double = grad*PI/180+min*PI/180/60+sec*PI/180/3600
 
 /**
  * Тривиальная
@@ -118,7 +118,7 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = initial*(sqr(1+per
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = number%10*100+number/10%10*10+number/100%10
+fun numberRevert(number: Int): Int = number%10*100+number/10%10*10+number/100
 
 
 
