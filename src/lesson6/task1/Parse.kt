843438cccf1c9732054a0e6f5b1df0ac961ade9e
vkyhrin <vkyhrin@gmail.com>
2018-12-19 00:01:32 +0300

    6 lesson

--- src/lesson6/task1/Parse.kt
+++ src/lesson6/task1/Parse.kt
@@ -120,7 +120,7 @@ fun dateDigitToStr(digital: String): String {
  * Все символы в номере, кроме цифр, пробелов и +-(), считать недопустимыми.
  * При неверном формате вернуть пустую строку
  */
-fun flattenPhoneNumber(phone: String): String = TODO()
+fun flattenPhoneNumber(phone: String): String =
 
 /**
  * Средняя
